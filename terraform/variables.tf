8bca91f2b074921b03a07cd92aba8711776fd1b7
geloman <geloman-likes-rust@users.noreply.github.com>
2024-09-23 14:00:46 +0800

    chors: upadate infra config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,9 +22,3 @@ variable "droplet_ssh_keys" {
   description = "digitalocean droplet ssh key fingerprints"
   type        = list(string)
 }
-
-variable "do_token" {
-  description = "digitalocean api access key"
-  type        = string
-  sensitive   = true
-}
