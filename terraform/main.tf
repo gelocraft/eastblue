64ea9694a6af2ad763145a6dbc990cde9cf97905
geloman <geloman-likes-rust@users.noreply.github.com>
2024-09-24 17:01:11 +0800

    update user_data

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,21 +6,21 @@ resource "digitalocean_droplet" "droplet" {
   region    = var.droplet_region
   size      = var.droplet_size
   ssh_keys  = var.droplet_ssh_keys
-  user_data = <<-EOT
+  user_data = <<-EOF
   #!/bin/sh
   apt update -y && apt upgrade -y
-  apt install -y zsh eza
-
-  # apt install -y zsh eza fzf ripgrep fd-find bat git-delta gcc tar unzip curl wget neofetch
+  apt install -y zsh neovim eza fzf ripgrep fd-find bat git-delta gcc tar unzip curl wget neofetch
 
   useradd --create-home --groups sudo,docker --shell /usr/bin/zsh geloman
   mkdir -p /home/geloman/{.config,.personal,.development}
+
   git clone --depth 1 https://github.com/geloman-likes-rust/dotfiles /home/geloman/.dotfiles
   git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git /home/geloman/.oh-my-zsh
+
   ln /home/geloman/.dotfiles/zsh/.zshrc /home/geloman/.zshrc
   ln /home/geloman/.dotfiles/nvim /home/geloman/.config/
   ln /home/geloman/.dotfiles/tmux/.tmux.conf /home/geloman/.tmux.conf
 
-  # nvim --headless "+Lazy! restore" +qa
-  EOT
+  nvim --headless "+Lazy! restore" +qa
+  EOF
 }
