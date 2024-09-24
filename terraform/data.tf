798bd04522323e57b9f29da470c3ee94e0e71c1b
geloman <geloman-likes-rust@users.noreply.github.com>
2024-09-24 21:29:39 +0800

    fix: undefined and wrong variable declaration

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,5 @@
 data "digitalocean_droplet_snapshot" "eastblue" {
-  name        = var.droplet_snapshot_name
+  name        = var.droplet_snapshot
   region      = var.droplet_region
   most_recent = true
 }
