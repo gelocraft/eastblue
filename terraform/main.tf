d0c42f076ea2b369b039bc2d72d9196223d8a25b
geloman <geloman-likes-rust@users.noreply.github.com>
2024-09-24 16:53:23 +0800

    update user_data

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,19 +7,20 @@ resource "digitalocean_droplet" "droplet" {
   size      = var.droplet_size
   ssh_keys  = var.droplet_ssh_keys
   user_data = <<-EOT
-    #!/bin/sh
-    apt update -y && apt upgrade -y
-    apt install -y zsh eza fzf ripgrep fd-find bat git-delta gcc tar unzip curl wget neofetch
-    useradd --create-home --groups sudo,docker --shell /usr/bin/zsh geloman
-    su geloman
-    pwd && whoami
-    mkdir -p ~/.config ~/.personal ~/.development
-    git clone --depth 1 https://github.com/geloman-likes-rust/dotfiles ~/.dotfiles
-    git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-    ln ~/.dotfiles/zsh/.zshrc ~/.zshrc
-    ln ~/.dotfiles/nvim ~/.config/
-    ln ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-    nvim --headless "+Lazy! restore" +qa
-    exit && reboot
+  #!/bin/sh
+  apt update -y && apt upgrade -y
+  apt install -y zsh eza
+
+  # apt install -y zsh eza fzf ripgrep fd-find bat git-delta gcc tar unzip curl wget neofetch
+
+  useradd --create-home --groups sudo,docker --shell /usr/bin/zsh geloman
+  mkdir -p /home/geloman/{.config,.personal,.development}
+  git clone --depth 1 https://github.com/geloman-likes-rust/dotfiles /home/geloman/.dotfiles
+  git clone --depth 1 https://github.com/ohmyzsh/ohmyzsh.git /home/geloman/.oh-my-zsh
+  ln /home/geloman/.dotfiles/zsh/.zshrc /home/geloman/.zshrc
+  ln /home/geloman/.dotfiles/nvim /home/geloman/.config/
+  ln /home/geloman/.dotfiles/tmux/.tmux.conf /home/geloman/.tmux.conf
+
+  # nvim --headless "+Lazy! restore" +qa
   EOT
 }
