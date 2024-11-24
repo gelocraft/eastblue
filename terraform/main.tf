2074f5b696049541598efe49b6004d97efd6e39c
geloman <geloman-likes-rust@users.noreply.github.com>
2024-11-25 01:47:32 +0800

    update machine image

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 provider "digitalocean" {}
 
 locals {
+  droplet_size = var.droplet_image
   droplet_snapshot = data.digitalocean_droplet_snapshot.eastblue.id
 }
 
 resource "digitalocean_droplet" "droplet" {
-  image    = local.droplet_snapshot
+  image    = local.droplet_image
   name     = var.droplet_name
   region   = var.droplet_region
   size     = var.droplet_size
