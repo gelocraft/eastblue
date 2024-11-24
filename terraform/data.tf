6ab2532a822e52b281284f40ddb8b2af1e6ff149
geloman <geloman-likes-rust@users.noreply.github.com>
2024-11-25 02:09:02 +0800

    update data resource config

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,5 @@
 data "digitalocean_droplet_snapshot" "eastblue" {
-  name        = var.droplet_snapshot
+  name_regex  = "^${var.droplet_snapshot}"
   region      = var.droplet_region
   most_recent = true
 }
