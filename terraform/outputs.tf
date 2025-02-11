b59adc149f2cbfdb30fb08cec67b09caad0eaffc
gelocraft <gelocraft@users.noreply.github.com>
2025-02-11 20:45:35 +0800

    chore: output public ssh fingerprint

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "droplet_ipv4_address" {
 output "droplet_size" {
   value = try(digitalocean_droplet.droplet.size, "")
 }
+
+output "ssh_fingerprint" {
+  value = try(element(digitalocean_droplet.droplet.ssh_keys, 0), "")
+}
