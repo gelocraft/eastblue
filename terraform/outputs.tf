b02da0f6555d146d74b20a579886982bbbc081ed
gelocraft <gelocraft@users.noreply.github.com>
2025-02-11 22:05:51 +0800

    chore: remove unused outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "droplet_ipv4_address" {
   value = digitalocean_droplet.droplet.ipv4_address
 }
-
-output "droplet_size" {
-  value = try(digitalocean_droplet.droplet.size, "")
-}
-
-output "ssh_fingerprint" {
-  value = try(element(digitalocean_droplet.droplet.ssh_keys, 0), "")
-}
